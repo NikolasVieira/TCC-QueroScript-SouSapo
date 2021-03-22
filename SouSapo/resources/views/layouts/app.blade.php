37c6495b0877b2f21105264825ba9d9a588c9116
Victor-Guzella <victor.guzella03@gmail.com>
2021-03-22 15:32:49 -0300

    Começo do Sobre
    Explicativo não?

--- SouSapo/resources/views/layouts/app.blade.php
+++ SouSapo/resources/views/layouts/app.blade.php
@@ -18,6 +18,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
 </head>
 <body>
     <div id="app">
@@ -45,7 +46,7 @@
                                     <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                                 </li>
                             @endif
-                            
+
                             @if (Route::has('register'))
                                 <li class="nav-item">
                                     <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
--- SouSapo/resources/views/sousapo/victor/sobre.blade.php
+++ SouSapo/resources/views/sousapo/victor/sobre.blade.php
@@ -1,7 +1,59 @@
 @extends('layouts.app')
-
 @section('content')
 
-html do sobre aqui
+<style>
+
+#ulSobre1 {
+    position: relative;
+    margin-left: 5px;
+    margin-right: 500px;
+    background-color: #CC99F4;
+}
+
+#divSobre1{
+    margin-left: 150px;
+}
+
+#divSobre2{
+    padding-top: 20px;
+    padding-bottom: 1px;
+}
+
+#nos {
+    font-size: 17px;
+}
+
+#divSobre11{
+    margin-left: 110px;
+}
+
+</style>
+
+<p>
+    <div id="divSobre1">
+        <h1>Sobre Nós</h1><br/>
+    </div>
+    <ul id="ulSobre1">
+        <div id="divSobre2">
+            <p id="nos">Maria Fernanda - WebDesign e Diretora Criativa</p>
+            <p id="nos">Victor Guzella - WebDesign e Programador</p>
+            <p id="nos">Nikolas Raposo - Programador e Roteirista</p>
+            <p id="nos">Micaeli Almeida - Escritora e Avaliadora</p>
+            <p id="nos">Kaelaine Pereira- Escritora e Avaliadora</p>
+            <p id="nos">Kayky Martins - DBA e Programador</p>
+        </div>
+    </ul>
+</p>
+<hr>
+<p>
+    <div id="divSobre11">
+        <h1>Sobre o Projeto</h1><br/>
+    </div>
+    <ul id="ulSobre1">
+        <div id="divSobre2">
+            <p id="">HQ</p>
+        </div>
+    </ul>
+</p>
 
 @endsection
