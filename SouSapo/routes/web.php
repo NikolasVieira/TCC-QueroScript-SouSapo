a99466428d7c43101c153fc1fa3e7e984026edaf
NikolasVieira <nikmap@hotmail.com>
2021-03-23 12:50:41 -0300

    Navbar feita
    Fiz os primeiros estagios da navbar

--- SouSapo/routes/web.php
+++ SouSapo/routes/web.php
@@ -45,9 +45,3 @@ Route::get('/laravel', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
