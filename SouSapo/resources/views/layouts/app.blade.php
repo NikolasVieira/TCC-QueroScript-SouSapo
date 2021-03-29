16503edc33182b79fe5325fde5229d0c455a26d9
NikolasVieira <nikmap@hotmail.com>
2021-03-29 11:58:17 -0300

    reimaginação da navbar
    Protótipo da navbar alterado

--- SouSapo/resources/views/layouts/app.blade.php
+++ SouSapo/resources/views/layouts/app.blade.php
@@ -25,76 +25,85 @@
 <body>
     <div id="app">
 
+
+
         <!-- NAVBAR -->
         <nav class="navbar navbar-expand-lg navbar-light" style="background-color: var(--sapo);">
-            <!-- Logo e Titulo da NavBar -->
-            <a id="logo" class="navbar-brand" href="{{ url('/') }}" style="background-color: var(--sapinho);">
-                <img src="https://a-static.mlcdn.com.br/618x463/quadro-infantil-desenho-sapo-conspecto/conspecto/148957/9b142972fa26b3c5f14c9d865d408650.jpg" width="30" height="30"
-                    class="d-inline-block align-top" alt="">
-                SouSapo
-            </a>
+            <div class="container-fluid">
+                <!-- Logo e Titulo da NavBar -->
+                <a id="logo" class="navbar-brand" href="{{ url('/') }}" style="background-color: var(--ra);">
+                    <img src="https://a-static.mlcdn.com.br/618x463/quadro-infantil-desenho-sapo-conspecto/conspecto/148957/9b142972fa26b3c5f14c9d865d408650.jpg"
+                        width="30" height="30" class="d-inline-block align-top" alt="">
+                    SouSapo
+                </a>
 
-            <!-- Navbar -->
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <!-- Botôes da navbar -->
-                <ul class="navbar-nav mr-auto">
-                    <li id="navbotao" class="nav-item active" style="background-color: var(--purple);">
-                        <a class="nav-link" href="{{ url('/hq') }}">Quadrinhos <span class="sr-only">(current)</span></a>
-                    </li>
-                    <li id="navbotao" class="nav-item active" style="background-color: var(--purple);">
-                        <a class="nav-link" href="{{ url('/apoio') }}">Apoiar <span class="sr-only">(current)</span></a>
-                    </li>
-                    <li id="navbotao" class="nav-item active" style="background-color: var(--purple);">
-                        <a class="nav-link" href="{{ url('/sobre') }}">Sobre <span class="sr-only">(current)</span></a>
-                    </li>
-                    <li id="navbotao" class="nav-item active" style="background-color: var(--purple);">
-                        <a class="nav-link" href="{{ url('/forum') }}">Fórum <span class="sr-only">(current)</span></a>
-                    </li>
-                    <li id="navbotao" class="nav-item active" style="background-color: var(--purple);">
-                        <a class="nav-link" href="{{ url('/comunidade') }}">Comunidade <span class="sr-only">(current)</span></a>
-                    </li>
-                </ul>
+                <!-- Navbar -->
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Botôes da navbar -->
+                    <ul class="navbar-nav mr-auto">
 
-                <!-- Lado direito na NavBar -->
-                <ul class="navbar-nav ml-auto">
-                    <!-- Links de autenticação -->
-                    @guest
+                        <li id="navbotao" class="nav-item active">
+                            <a class="btn btn-sapinho" href="{{ url('/hq') }}" role="button">Quadrinhos</a>
+                        </li>
 
-                    @if (Route::has('login'))
-                    <li class="nav-item" style="background-color: var(--purple);">
-                        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                    </li>
-                    @endif
+                        <li id="navbotao" class="nav-item active">
+                            <a class="btn btn-sapinho" href="{{ url('/sobre') }}" role="button">Sobre</a>
+                        </li>
 
-                    @if (Route::has('register'))
-                    <li class="nav-item" style="background-color: var(--purple);">
-                        <a class="nav-link" href="{{ route('register') }}">{{ __('Registrar') }}</a>
-                    </li>
-                    @endif
+                        <li id="navbotao" class="nav-item active">
+                            <a class="btn btn-sapinho" href="{{ url('/apoio') }}" role="button">Apoiar</a>
+                        </li>
 
-                    @else
-                    <li class="nav-item dropdown">
-                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre style="background-color: var(--purple);">
-                            {{ Auth::user()->name }}
-                        </a>
+                        <li id="navbotao" class="nav-item active">
+                            <a class="btn btn-sapinho" href="{{ url('/apoio') }}" role="button">Apoiar</a>
+                        </li>
 
-                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
-                                                 document.getElementById('logout-form').submit();">
-                                {{ __('Sair') }}
+                        <li id="navbotao" class="nav-item active">
+                            <a class="btn btn-sapinho" href="{{ url('/comunidade') }}" role="button">Comunidade</a>
+                        </li>
+                    </ul>
+
+                    <!-- Lado direito na NavBar -->
+                    <ul class="navbar-nav ml-auto">
+                        <!-- Links de autenticação -->
+                        @guest
+
+                        @if (Route::has('login'))
+                        <li id="navbotao" class="nav-item">
+                            <a class="btn btn-ra" href="{{ route('login') }}">{{ __('Login') }}</a>
+                        </li>
+                        @endif
+
+                        @if (Route::has('register'))
+                        <li id="navbotao" class="nav-item">
+                            <a class="btn btn-ra" href="{{ route('register') }}">{{ __('Registrar') }}</a>
+                        </li>
+                        @endif
+
+                        @else
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="btn btn-ra dropdown-toggle" href="#" role="button"
+                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                {{ Auth::user()->name }}
                             </a>
 
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                @csrf
-                            </form>
-                        </div>
-                    </li>
-                    @endguest
-                </ul>
-            </div>
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                 document.getElementById('logout-form').submit();">
+                                    {{ __('Sair') }}
+                                </a>
 
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                    @csrf
+                                </form>
+                            </div>
+                        </li>
+                        @endguest
+                    </ul>
+                </div>
+            </div>
         </nav>
+
         <!-- CONTEUDO -->
         <main class="pb-5">
             @yield('content')
@@ -102,9 +111,7 @@
 
         <!-- FOOTER -->
         <div class="footer">
-
             <p>Footer</p>
-
         </div>
 
     </div>
