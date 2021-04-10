d032e5bf2bb61fb5d128ceb7c97020b3c515a0f0
NikolasVieira <nikmap@hotmail.com>
2021-04-10 19:16:16 -0300

    Footer Inicial Feito
    Fiz um Footer totalmente responsivo com css separado

--- SouSapo/resources/views/layouts/app.blade.php
+++ SouSapo/resources/views/layouts/app.blade.php
@@ -22,6 +22,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/footer.css') }}" rel="stylesheet">
 
 </head>
 
@@ -112,63 +113,66 @@
         </nav>
 
         <!-- CONTEUDO -->
-        <main class="pb-5">
+        <main class="pb-0">
             @yield('content')
         </main>
 
         <!-- FOOTER -->
 
-        <footer>
-            <div class="container">
-                <div class="sec sobrenos">
-                    <h2>Sobre Nós</h2>
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus nam odio magni incidunt iste
-                        dolore molestias, hic ipsa porro obcaecati reprehenderit, repudiandae nulla eaque deleniti
-                        recusandae dignissimos id omnis corrupti?</p>
-                    <ul class="sci">
-                        <li><a class="facebook" href="#"><i class="fa fa-facebook-official" aria-hidden="true"></i>
-                            </a></li>
-                        <li><a class="instagram" href="#"><i class="fa fa-instagram" aria-hidden="true"></i>
-                            </a></li>
-                        <li><a class="twitter" href="#"><i class="fa fa-twitter" aria-hidden="true"></i>
-                            </a></li>
-                        <li><a class="youtube" href="#"><i class="fa fa-youtube-play" aria-hidden="true"></i>
-                            </a></li>
-                    </ul>
+        <div id="footer">
+            <footer>
+                <div class="container">
+                    <div class="sec sobrenos">
+                        <h2>Sobre Nós</h2>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus nam odio magni incidunt
+                            iste
+                            dolore molestias, hic ipsa porro obcaecati reprehenderit, repudiandae nulla eaque deleniti
+                            recusandae dignissimos id omnis corrupti?</p>
+                        <ul class="sci">
+                            <li><a class="facebook" href="#"><i class="fa fa-facebook-official" aria-hidden="true"></i>
+                                </a></li>
+                            <li><a class="instagram" href="#"><i class="fa fa-instagram" aria-hidden="true"></i>
+                                </a></li>
+                            <li><a class="twitter" href="#"><i class="fa fa-twitter" aria-hidden="true"></i>
+                                </a></li>
+                            <li><a class="youtube" href="#"><i class="fa fa-youtube-play" aria-hidden="true"></i>
+                                </a></li>
+                        </ul>
+                    </div>
+                    <div class="sec quickLinks">
+                        <h2>Links Rápidos</h2>
+                        <ul>
+                            <li><a href="{{ url('/') }}">Inicio</a></li>
+                            <li><a href="{{ url('/hq') }}">Quadrinhos</a></li>
+                            <li><a href="{{ url('/sobre') }}">Sobre</a></li>
+                            <li><a href="{{ url('/apoio') }}">Apoiar</a></li>
+                            <li><a href="{{ url('/forum') }}">Fórum</a></li>
+                            <li><a href="{{ url('/comunidade') }}">Comunidade</a></li>
+                        </ul>
+                    </div>
+                    <div class="sec contato">
+                        <h2>Informações e Contato</h2>
+                        <ul class="info">
+                            <li>
+                                <span><i class="fa fa-map-marker" aria-hidden="true"></i></span>
+                                <span>R. Lúcio Sarti, 809 <br>Parque Eldorado, Bebedouro <br>SP, 14706-120</span>
+                            </li>
+                            <li>
+                                <span><i class="fa fa-phone" aria-hidden="true"></i></span>
+                                <p><a href="tel:551733439695">+55 (17)3343-9695</a><br>
+                                    <a href="tel:12345678900">+1 (23)4567-8900</a></p>
+                            </li>
+                            <li>
+                                <span><i class="fa fa-envelope" aria-hidden="true"></i></span>
+                                <p><a href="mailto:quemleuperdeu@hotmail.com">quemleuperdeu@hotmail.com</a></p>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
-                <div class="sec quickLinks">
-                    <h2>Links Rápidos</h2>
-                    <ul>
-                        <li><a href="{{ url('/') }}">Inicio</a></li>
-                        <li><a href="{{ url('/hq') }}">Quadrinhos</a></li>
-                        <li><a href="{{ url('/sobre') }}">Sobre</a></li>
-                        <li><a href="{{ url('/apoio') }}">Apoiar</a></li>
-                        <li><a href="{{ url('/forum') }}">Fórum</a></li>
-                        <li><a href="{{ url('/comunidade') }}">Comunidade</a></li>
-                    </ul>
-                </div>
-                <div class="sec contato">
-                    <h2>Informações e Contato</h2>
-                    <ul class="info">
-                        <li>
-                            <span><i class="fa fa-map-marker" aria-hidden="true"></i></span>
-                            <span>R. Lúcio Sarti, 809 <br>Parque Eldorado, Bebedouro <br>SP, 14706-120</span>
-                        </li>
-                        <li>
-                            <span><i class="fa fa-phone" aria-hidden="true"></i></span>
-                            <p><a href="tel:551733439695">+55 (17)3343-9695</a><br>
-                                <a href="tel:12345678900">+1 (23)4567-8900</a></p>
-                        </li>
-                        <li>
-                            <span><i class="fa fa-envelope" aria-hidden="true"></i></span>
-                            <p><a href="mailto:quemleuperdeu@hotmail.com">quemleuperdeu@hotmail.com</a></p>
-                        </li>
-                    </ul>
-                </div>
+            </footer>
+            <div class="copyrightText">
+                <p>Copyright © 2021 QueroScripts. All Rights Reserved.</p>
             </div>
-        </footer>
-        <div class="copyrightText">
-            <p>Copyright © 2021 QueroScripts. All Rights Reserved.</p>
         </div>
 
     </div>
