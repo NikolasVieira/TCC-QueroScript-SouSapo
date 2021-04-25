7ae8825b410cf02e99d5262906f1d41bd3fdb879
NikolasVieira <nikmap@hotmail.com>
2021-04-25 15:28:26 -0300

    Update index.blade.php

--- SouSapo/resources/views/sousapo/nikolas/hq/index.blade.php
+++ SouSapo/resources/views/sousapo/nikolas/hq/index.blade.php
@@ -6,14 +6,14 @@
     <hr />
     <a href="{{ route('hq.create') }}" class="btn btn-success">Novo Capitulo</a>
     <br><br>
-    <div class="row">
+    <div class="row row-cols-3">
         @foreach($hq as $hqs)
-        <div class="card col" style="width: 18rem;">
-            <img src="img/hq_marvel_capa" class="card-img-top" alt="...">
+        <div class="card text-white bg-dark">
+            <img src="{{ asset('img/hq_marvel_capa.jpg') }}" class="card-img-top" alt="...">
             <div class="card-body">
                 <h5 class="card-title">{{ $hqs->hq_name }}</h5>
-                <p class="card-text">Id do Capitulo:{{ $hqs->id }}<br>Numero do
-                    Capitulo:{{ $hqs->chapter_number }}<br>Numero de Paginas:{{ $hqs->page_number }}</p>
+                <p class="card-text">Id do Capitulo: {{ $hqs->id }}<br>Numero do
+                    Capitulo: {{ $hqs->chapter_number }}<br>Numero de Paginas: {{ $hqs->page_number }}</p>
                 <a href="{{ route('hq.edit',$hqs->id)}}" class="btn btn-primary">Editar</a>
             </div>
         </div>
