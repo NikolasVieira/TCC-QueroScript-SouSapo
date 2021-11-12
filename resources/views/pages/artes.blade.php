fdd44b3c1b1fec2c3d164a274e5be7f4f7a63322
Nikolas <62063505+NikolasVieira@users.noreply.github.com>
2021-11-12 14:43:35 -0300

    atualização na pagina Artes
    Para adicionar uma arte agora é por uma modal

--- resources/views/pages/artes.blade.php
+++ resources/views/pages/artes.blade.php
@@ -1,48 +1,77 @@
 @extends('layouts.app')
 @section('content')
     <div>
-        <div class="col p-5 bg-razinha" id="Criar_Topico">
-            <h1>Adicionar Arte</h1>
-            <form method="POST" action="{{ route('sousapo.artes.store') }}" enctype="multipart/form-data">
-                @csrf
-                <div class="row">
-                    <input class="col m-1" type="text" name="titulo" id="titulo" placeholder="titulo">
-                    @error('titulo') <span class="error">É nescessario titulo!</span> @enderror
-                    <input class="col m-1" type="text" name="descricao" id="descricao" placeholder="descricao">
-                    @error('descricao') <span class="error">É nescessario preencher o conteudo!</span>
-                    @enderror
-                    <input type="file" name="image" id="image">
-                    @error('image')
-                        {{ $message }}
-                    @enderror
-                </div>
-                <button class="btn btn-ra" style="width: 10rem" type="submit">Salvar</button>
-            </form>
-        </div>
-        <hr>
         <div class="container">
+            <br>
             <h1>Artes da Comunidade</h1>
-            <div class="d-flex">
-                @if (isset($filtro))
-                    <form class="d-flex" method="GET" action="{{ route('sousapo.artes.index') }}">
 
-                        <input style="display: none" class="form-control me-2" type="search" placeholder="filtro"
-                            aria-label="filtro" id="filtro" name="filtro" value="">
-                        <button class="btn btn-success me-2" type="submit">Limpar busca</button>
-                    </form>
-                @endif
-                <form class="d-flex" method="GET" action="{{ route('sousapo.artes.index') }}">
+            <div class="row">
+                <div class="col">
+                    <div class="d-flex">
+                        @if (isset($filtro))
+                            <form class="d-flex" method="GET" action="{{ route('sousapo.artes.index') }}">
+                                <input style="display: none" class="form-control me-2" type="search" placeholder="filtro"
+                                    aria-label="filtro" id="filtro" name="filtro" value="">
+                                <button class="btn btn-ra me-2" type="submit">Limpar busca</button>
+                            </form>
+                        @endif
 
-                    <input class="form-control me-2" type="search" placeholder="Pesquisar.." aria-label="filtro" id="filtro"
-                        name="filtro">
-                    <button class="btn btn-success" type="submit">Procurar</button>
-                </form>
+                        <form class="d-flex" method="GET" action="{{ route('sousapo.artes.index') }}">
+                            <input class="form-control me-2" type="search" placeholder="Pesquisar.." aria-label="filtro"
+                                id="filtro" name="filtro">
+                            <button class="btn btn-ra" type="submit">Procurar</button>
+                        </form>
+                    </div>
+                </div>
+                <div class="col-2">
+                    <!-- Button trigger modal -->
+                    <button type="button" class="btn btn-ra" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                        Adicionar Arte
+                    </button>
+                </div>
             </div>
-            <div>
-                <button aling="right">Filtro</button>
-                <button aling="center">Artes Originais</button>
 
+            <!-- Modal -->
+            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel"
+                aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content p-4">
+                        <form method="POST" action="{{ route('sousapo.artes.store') }}" enctype="multipart/form-data">
+                            <div class="modal-header">
+                                <h5 class="modal-title">Adicionar Arte</h5>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                    aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
+                                @csrf
+                                <div class="row">
+                                    <input class="col form-control mr-1" type="text" name="titulo" id="titulo"
+                                        placeholder="Titulo">
+                                    @error('titulo')
+                                    <span class="error">É nescessario titulo!</span>
+                                    @enderror
+
+                                    <input class="col form-control ml-1 " type="text" name="descricao" id="descricao"
+                                        placeholder="Descricao">
+                                    @error('descricao')
+                                    <span class="error">É nescessario preencher o conteudo!</span>
+                                    @enderror
+
+                                    <input class="form-control mt-2" type="file" id="image" name="image">
+                                    @error('image')
+                                        {{ $message }}
+                                    @enderror
+                                </div>
+                            </div>
+                            <div class="modal-footer">
+                                <button class="btn btn-ra" style="width: 10rem" type="submit">Salvar</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
             </div>
+
+            <!-- Artes -->
             <hr>
             <div class="row row-cols-1 row-cols-md-3 g-4">
                 @foreach ($arte as $artes)
@@ -81,6 +110,7 @@
                     </div>
                 @endforeach
             </div>
+            <br>
         </div>
     </div>
 @endsection
